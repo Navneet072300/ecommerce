be85e386ca933f9a68be13fd713d4cc202c63489
Navneet Shahi <navneetshahi345@gmail.com>
2025-03-30 18:57:58 +0530

    update NavBar.jsx and CardDrawer.jsx

--- src/components/Common/NavBar.jsx
+++ src/components/Common/NavBar.jsx
@@ -1,17 +1,22 @@
 import { FaUser } from "react-icons/fa6";
 import { IoBag } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2, RxHamburgerMenu } from "react-icons/rx";
 import SearchBar from "./SearchBar";
 import CardDrawer from "../Layout/CardDrawer";
 import { useState } from "react";
 
 const NavBar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
 
   const toggleCartDrawer = () => {
     setDrawerOpen(!drawerOpen);
   };
+  const toggleNavDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
+
   return (
     <>
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
@@ -64,12 +69,61 @@ const NavBar = () => {
             <SearchBar />
           </div>
 
-          <button className="md:hidden cursor-pointer">
+          <button
+            onClick={toggleNavDrawer}
+            className="md:hidden cursor-pointer"
+          >
             <RxHamburgerMenu className="h-6 w-6 text-gray-700" />
           </button>
         </div>
       </nav>
       <CardDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full transition-transform
+         bg-white shadow-lg transform duration-300 z-50 ${
+           navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+         }`}
+      >
+        <div className="flex justify-end p-4">
+          <button className="" onClick={toggleNavDrawer}>
+            <RxCross2 className="h-6 w-6 text-gray-600" />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="flex flex-col space-y-4">
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Men
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Women
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Bottom Wear
+            </Link>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Layout/CardDrawer.jsx
+++ src/components/Layout/CardDrawer.jsx
@@ -1,9 +1,10 @@
 import { RxCross2 } from "react-icons/rx";
+import CartComponents from "../Cart/CartComponents";
 
 const CardDrawer = ({ toggleCartDrawer, drawerOpen }) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300
+      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300
         flex flex-col z-50 ${
           drawerOpen ? "translate-x-0" : "translate-x-full"
         }`}
@@ -12,6 +13,20 @@ const CardDrawer = ({ toggleCartDrawer, drawerOpen }) => {
         <button onClick={toggleCartDrawer} className="text-2xl cursor-pointer">
           <RxCross2 className="h-6 w-6 text-gray-600" />
         </button>
+      </div>
+
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        <CartComponents />
+      </div>
+
+      <div className="p-4 sticky bottom-0 bg-white">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
+          Checkout
+        </button>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+          Shipping, taxes, and discount codes calculated at checkout
+        </p>
       </div>
     </div>
   );
