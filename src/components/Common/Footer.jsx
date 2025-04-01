ffa486ccc232746db7e5dfccb79856c518409d74
Navneet Shahi <navneetshahi345@gmail.com>
2025-04-01 12:09:16 +0530

    update Footer.jsx

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,3 +1,12 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaRegCopyright,
+  FaXTwitter,
+} from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { FaPhoneAlt } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="border-t py-12">
@@ -8,7 +17,9 @@ const Footer = () => {
             Be the first to hear about new products, exclusive events, and
             online offers
           </p>
-          <p className="">Sign up and get 10% off on your first order</p>
+          <p className="font-medium text-sm text-gray-600 mb-6">
+            Sign up and get 10% off on your first order
+          </p>
 
           <form action="" className="flex">
             <input
@@ -27,6 +38,97 @@ const Footer = () => {
             </button>
           </form>
         </div>
+
+        <div className="">
+          <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+          <ul className="space-y-2 text-gray-600">
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Men's Top Wear
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Women's Top Wear
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Men's Bottom Wear
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Women's Bottom Wear
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="">
+          <h3 className="text-lg text-gray-800 mb-4">Support</h3>
+          <ul className="space-y-2 text-gray-600">
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                FAQ's
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:text-gray-500 transition-colors">
+                Features
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="">
+          <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
+          <div className="flex items-center space-x-4 mb-6">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <FaFacebook className="h-5 w-5" />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <FaInstagram className="h-5 w-5" />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-500"
+            >
+              <FaXTwitter className="h-4 w-4" />
+            </a>
+          </div>
+          <p className="text-gray-500 ">Call Us</p>
+          <p className="">
+            <FaPhoneAlt className="inline-block mr-2" />
+            +1 0123-456-789
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6">
+        <p className="text-gray-500 text-sm tracking-tighter text-center">
+          ©2025, CompileTab. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
