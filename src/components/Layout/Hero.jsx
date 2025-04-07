8253c7fbc696f02202cec3143f58c7ca86b5e1b0
Navneet Shahi <navneetshahi345@gmail.com>
2025-04-07 20:24:02 +0530

    update Hero.jsx

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -1,4 +1,6 @@
+// @ts-nocheck
 // @ts-ignore
+import { Link } from "react-router-dom";
 import heroImg from "../../assets/rabbit-hero.webp";
 
 const Hero = () => {
@@ -12,8 +14,17 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/5 flex items-center justify-center">
         <div className="text-center text-white p-6">
           <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
-            Vacation
+            Vacation <br /> Ready
           </h1>
+          <p className="text-sm tracking-tighter md:text-lg mb-6">
+            Explore our vacation ready fits with fast worldwide delivery.
+          </p>
+          <Link
+            to="#"
+            className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+          >
+            Shop Now
+          </Link>
         </div>
       </div>
     </section>
