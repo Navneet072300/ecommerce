f4184500620717099a4bf371af92c7a8430bdab7
Navneet Shahi <navneetshahi345@gmail.com>
2025-05-10 18:04:37 +0530

    update AdminSidebar.jsx

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -1,13 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { FaSignOutAlt } from "react-icons/fa";
+import { FaUser, FaBox, FaStore, FaClipboardList } from "react-icons/fa6";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const links = [
+  { to: "/admin/users", label: "Users", icon: <FaUser /> },
+  { to: "/admin/products", label: "Products", icon: <FaBox /> },
+  { to: "/admin/orders", label: "Orders", icon: <FaClipboardList /> },
+  { to: "/admin/shop", label: "Shop", icon: <FaStore /> },
+];
 
 const AdminSidebar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/");
+  };
+
   return (
     <div className="p-6">
       <div className="mb-6">
         <Link to="/admin" className="text-2xl font-medium">
           Rabbit
         </Link>
+      </div>
+
+      <h2 className="text-xl font-medium mb-6 text-center">Admin Dashboard</h2>
+
+      <nav className="flex flex-col space-y-2">
+        {links.map(({ to, label, icon }) => (
+          <NavLink
+            key={to}
+            to={to}
+            className={({ isActive }) =>
+              isActive
+                ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
+                : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+            }
+          >
+            {icon}
+            <span>{label}</span>
+          </NavLink>
+        ))}
+      </nav>
+      <div className="mt-6">
+        <button
+          onClick={handleLogout}
+          className="w-full bg-red-500 hover:bg-red-600
+        text-white py-2 px-4 rounded flex items-center justify-center space-x-2 cursor-pointer"
+        >
+          <FaSignOutAlt />
+          <span className="">Logout</span>
+        </button>
       </div>
     </div>
   );
